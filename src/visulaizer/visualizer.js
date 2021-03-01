e6d416ef8944996d2fa13257646b45e35ffc30bf
Aman Singh <amansinghrathhore117@gmail.com>
2021-03-02 00:12:00 +0530

    added walls and made start and end points

--- src/visulaizer/visualizer.js
+++ src/visulaizer/visualizer.js
@@ -1,51 +1,200 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'; 
+
+// import { dijkstra, getNodesInShortestPathOrder } from '../algorithms/dijkstra';
 import Node from '../node/node'; 
+
 import "./visualizer.css";
+
+
+let START_NODE_ROW = 5;
+let START_NODE_COL = 5;
+let FINISH_NODE_ROW = 15;
+let FINISH_NODE_COL = 15; 
+
+
 export default class Visualizer extends Component {
     state = {  
-        grid: [],
+        grid: [], 
+        startchange: false,
+        endchange: false, 
       mouseIsPressed: false,
-     } 
-Node
+     }  
+
+
      componentDidMount() {
         const grid = getInitialGrid();
         this.setState({grid});
-      }
+      } 
 
-    render() {  
-        const {grid}= this.state;
-        return (   
+      
+  handleMouseDown=(row, col)=> { 
+    if( row === START_NODE_ROW && col === START_NODE_COL) 
+     {    this.setState({startchange:true,mouseIsPressed: true});   
+     return;  }
+    if( row === FINISH_NODE_ROW && col === FINISH_NODE_COL)  
+    {  
+      this.setState({endchange:true,mouseIsPressed: true});   
+     return;  }
+    const newGrid = getNewGridWithWallToggled(this.state.grid, row, col);
+    this.setState({grid: newGrid, mouseIsPressed: true});
+  } 
+
+  handleMouseEnter=(row, col) =>{
+    if (!this.state.mouseIsPressed) return; 
+    const{ startchange, endchange}= this.state;  
+    if(startchange) 
+    { 
+      const newGrid =changeStartPoint(this.state.grid, row, col, 1);
+      this.setState({grid: newGrid});
+      START_NODE_ROW = row;
+      START_NODE_COL = col;  
+      const newstGrid =changeStartPoint(this.state.grid, row, col, 0, 1);
+      this.setState({grid: newstGrid});
+      
+    }  
+    else if(endchange) 
+    { 
+      const newGrid =changeStartPoint(this.state.grid, row, col, 0,0,1);
+      this.setState({grid: newGrid});
+     FINISH_NODE_ROW = row;
+      FINISH_NODE_COL = col;  
+      const newstGrid =changeStartPoint(this.state.grid, row, col, 0,0, 0, 1);
+      this.setState({grid: newstGrid});
+      
+    } 
+   else { const newGrid = getNewGridWithWallToggled(this.state.grid, row, col);
+    this.setState({grid: newGrid}); }
+  }
+
+  handleMouseUp=(row,col)=> { 
+    const{ startchange, endchange}= this.state; 
+    if( startchange) { this.setState({startchange:false,mouseIsPressed: false}); }
+  else  if( endchange) this.setState({endchange:false,mouseIsPressed: false}); 
+    else  this.setState({mouseIsPressed: false}); 
+  
+  }
+
+    render() {   
+      
+        const {grid,mouseIsPressed}= this.state;
+        return (     
+          <div>
+          <button className=" btn btn-primary mt-2" onClick={() => this.visualizeDijkstra()}>
+          Visualize Dijkstra's Algorithm
+        </button>
             <div className="grid">
             {grid.map((row, rowIdx) => {
-              return (
+              return ( 
+
                 <div key={rowIdx} className="gridRow">
                   {row.map((node, nodeIdx) => {
-                    // const {row, col, isFinish, isStart, isWall} = node;
-                    return ( 
-                      
-                      <Node
-                        key={nodeIdx}
-                        ></Node>
+                     const {row, col, isFinish, isStart, isWall} = node; 
+                    // console.log( node);
+                     return (
+                       <Node
+                       key={nodeIdx} 
+                       row={row}
+                       col={col}
+                       isFinish={isFinish}
+                       isStart={isStart}
+                       isWall={isWall}
+                       mouseIsPressed={mouseIsPressed}
+                       onMouseDown={(row, col) => this.handleMouseDown(row, col)}
+                       onMouseEnter={(row, col) => 
+                        {return  this.handleMouseEnter(row, col)}}
+                       onMouseUp={(row, col) => { return this.handleMouseUp(row,col)}}
+                       ></Node>
                     );
                   })}
                 </div>
               );
             })}
           </div>
-
+          </div>
          );
     }
 }
- 
+ // row= 60, col= 20  
+
  
 const getInitialGrid = () => {
     const grid = [];
     for (let row = 0; row < 60; row++) {
       const currentRow = [];
       for (let col = 0; col < 20; col++) {
-        currentRow.push([]);
+        currentRow.push(createNode(col, row));
       }
       grid.push(currentRow);
     }
     return grid;
+  }; 
+
+  
+const createNode = (col, row) => {
+  return {
+    col,
+    row,
+    isStart: row === START_NODE_ROW && col === START_NODE_COL,
+    isFinish: row === FINISH_NODE_ROW && col === FINISH_NODE_COL,
+    distance: Infinity,
+    isVisited: false,
+    isWall: false,
+    previousNode: null,
   };
+}; 
+
+const getNewGridWithWallToggled = (grid, row, col) => {
+  const newGrid = grid.slice();
+  const node = newGrid[row][col];  
+   let newNode= {};
+   newNode = {
+    ...node,
+    isWall: !node.isWall, 
+  }; 
+ newGrid[row][col] = newNode; 
+  return newGrid;
+}; 
+
+
+const changeStartPoint = (grid, row, col, ps=0, ns=0, pe=0,ne=0) => {
+  const newGrid = grid.slice();  
+  const startNode = newGrid[START_NODE_ROW][START_NODE_COL];  
+  const endNode = newGrid[FINISH_NODE_ROW][FINISH_NODE_COL];
+   let newNode= {};
+   let previousNode= {};
+  if(ps)  
+  {
+    previousNode = {
+      ...startNode,
+      isStart: false
+    }; 
+    newGrid[START_NODE_ROW][START_NODE_COL]=previousNode;
+   }  
+   if(pe)  
+  {
+    previousNode = {
+      ...endNode,
+      isFinish: false
+    }; 
+    newGrid[FINISH_NODE_ROW][FINISH_NODE_COL]=previousNode;
+   } 
+   if(ns)  
+   {
+     newNode = {
+       ...startNode,
+       isStart: true
+     }; 
+     newGrid[START_NODE_ROW][START_NODE_COL]=newNode;
+    }  
+    if(ne)  
+  {
+    newNode = {
+      ...endNode,
+      isFinish: true
+    }; 
+    newGrid[FINISH_NODE_ROW][FINISH_NODE_COL]=newNode;
+   } 
+     
+  
+  return newGrid;
+};
